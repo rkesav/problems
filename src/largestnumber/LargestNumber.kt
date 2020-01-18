166a7cee35257316aedf376843b4ec62384bd486
rkesav <rkesav1990@gmail.com>
2020-01-18 07:33:05 +0530

    Code for largest number. Not completed. Only logic.

---
+++ src/largestnumber/LargestNumber.kt
@@ -0,0 +1,16 @@
+package largestnumber
+
+class LargestNumber {
+
+    fun largestNumber(nums: IntArray): String {
+        val map = mutableMapOf<Int, IntArray>()
+
+        // Implement comparator by concatenating two number string in both orders and comparing each other
+    }
+}
+
+fun main() {
+    //val result = MaxGap().maximumGap(arrayOf(3, 6, 9, 1).toIntArray())
+    val result = LargestNumber().largestNumber(arrayOf(1,1,1,1,1,5,5,5,5,5).toIntArray())
+    println(result)
+}
